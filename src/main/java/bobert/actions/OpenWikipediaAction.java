d56c2fcd1450e3322b67381cfb35fbd5d884c814
James Wang <007jcw@gmail.com>
2022-03-29 00:21:22 -0400

    replace hello world text with lorem ipsum

---
+++ src/main/java/bobert/actions/OpenWikipediaAction.java
@@ -0,0 +1,10 @@
+package bobert.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+
+public class OpenWikipediaAction extends AbstractGameAction {
+    @Override
+    public void update() {
+        isDone = true;
+    }
+}
